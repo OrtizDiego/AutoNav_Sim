dc92df684df74c624b73ec3c56ed731a8837dc31
OrtizDiego <diego.ortiz@tum.de>
2026-01-22 18:16:59 +0100

    feat: create the bridge to gazebo and launch file

---
+++ src/my_bot/launch/sim.launch.py
@@ -0,0 +1,38 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_name = 'my_bot'
+
+    # 1. Start the Robot State Publisher (RSP)
+    # This processes the URDF and publishes the robot state
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(pkg_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # 2. Start Gazebo
+    # We include the standard gazebo_ros launch file
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
+        )])
+    )
+
+    # 3. Spawn the Robot
+    # This node simply asks Gazebo to "spawn" the entity published by RSP
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
+
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
